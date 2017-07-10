5fa537ecd1271937bb6bf6c04163b3da4463eaf6
Joe <joenardone25@gmail.com>
2017-07-10 02:25:26 -0400

    Commented out depreciated functions to avoid compile errors

--- Capstone_Vision_Test/stop_marker_detection.cpp
+++ Capstone_Vision_Test/stop_marker_detection.cpp
@@ -154,63 +154,63 @@ void drawBoundingBoxes_wNumbers(cv::Mat binImg, cv::Mat& outImg, double objWidth
 
 
 // FUNCTIONS BELOW ARE DEPRECIATED
-// returns distance of object from camera
-double get_objDist(cv::Mat imorig, double objWidth, double foclen)
-{
-	// Declarations
-	cv::Mat imraw = imorig.clone(); // Clone the image to avoid damage on the original 
-	double objDist = 0; // return variable
-	double objPixelWidth = 0; // pixel width of obj in image to be found
-
-	objPixelWidth = detect_rectWidth_contour(imraw); // get width of the rectangle in pixels
-	objDist = objDetectDist(objWidth, objPixelWidth, foclen); // get distance from camera
-
-															  // Close All Windows
-	cv::destroyAllWindows();
-
-	return objDist; // return distance
-}
-
-// detect rectange in image and return the horizontal width using contours
-double detect_rectWidth_contour(cv::Mat im)
-{
-	// Declarations
-	double rectWidth = 0;								// return variable
-	std::vector<std::vector<cv::Point>> contour_arr;	// store detected contours
-	std::vector<cv::Vec4i> hierarch;					// hierarchy array for findContours
-	cv::Mat imdrawContour = im.clone();					// clone of orignal image for drawing contours on
-	cv::Mat imRect = im.clone();						// clone of orignal image for drawing rectangles on
-	cv::Rect detectedRect;								// define rectange object
-	int tilesize = 16;									// tile size of texture classifier (needs to become function argument)
-	resolution origRes = { 2592, 1458 };					// orignal resolution (needs to become function arguement)
-
-															// Step 1: Find contours in image
-	cv::cvtColor(im, im, cv::COLOR_BGR2GRAY);			// Convert BGR to grayscale findContours
-	cv::findContours(im, contour_arr, hierarch, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE); // find all contours in image (alt methods available)
-
-																						   // Step 2: Find Bounds and Draw on Image
-	detectedRect = cv::boundingRect(contour_arr[0]);	// get the bounding rectange dimensions from first contour found
-	cv::resize(imRect, imRect, cv::Size(origRes.width, origRes.height));	// resize image to original size
-	cv::Rect scaledRect(detectedRect.x*tilesize, detectedRect.y*tilesize, detectedRect.width*tilesize, detectedRect.height*tilesize); // scale detect rectangle to new resolution for distance calc
-																																	  // Draw Bounding Rectangle on Image
-	cv::rectangle(imRect, scaledRect, cv::Scalar(0, 0, 255), 3, cv::LINE_8, 0);
-	cv::Mat imRectView = pyrDownDef(imRect, 3);
-	cv::imshow("Bounding Rectangle", imRectView);
-	cv::waitKey(0);
-
-	// Step 2 Alternate Display: Draw All Contours
-	cv::drawContours(imdrawContour, contour_arr, 0, cv::Scalar(0, 0, 255), 3, cv::LINE_8, hierarch);
-	cv::resize(imdrawContour, imdrawContour, cv::Size(origRes.width, origRes.height));	// resize image to original size
-	cv::Mat imContourView = pyrDownDef(imdrawContour, 3);
-	cv::imshow("Detected Contours Drawn On Image", imContourView);
-	cv::waitKey(0);
-	// Draw Multiple Contours Detected
-	//int idx = 0;
-	//for (; idx >= 0; idx = hierarch[idx][0])
-	// cv::drawContours(imdraw, contour_arr, idx, cv::Scalar(0,0,255), 3, 8, hierarch);
-
-	// Close All Windows
-	cv::destroyAllWindows();
-
-	return rectWidth = scaledRect.width; // returned scaled width to match with focal length
-}
+//// returns distance of object from camera
+//double get_objDist(cv::Mat imorig, double objWidth, double foclen)
+//{
+//	// Declarations
+//	cv::Mat imraw = imorig.clone(); // Clone the image to avoid damage on the original 
+//	double objDist = 0; // return variable
+//	double objPixelWidth = 0; // pixel width of obj in image to be found
+//
+//	objPixelWidth = detect_rectWidth_contour(imraw); // get width of the rectangle in pixels
+//	objDist = objDetectDist(objWidth, objPixelWidth, foclen); // get distance from camera
+//
+//															  // Close All Windows
+//	cv::destroyAllWindows();
+//
+//	return objDist; // return distance
+//}
+//
+//// detect rectange in image and return the horizontal width using contours
+//double detect_rectWidth_contour(cv::Mat im)
+//{
+//	// Declarations
+//	double rectWidth = 0;								// return variable
+//	std::vector<std::vector<cv::Point>> contour_arr;	// store detected contours
+//	std::vector<cv::Vec4i> hierarch;					// hierarchy array for findContours
+//	cv::Mat imdrawContour = im.clone();					// clone of orignal image for drawing contours on
+//	cv::Mat imRect = im.clone();						// clone of orignal image for drawing rectangles on
+//	cv::Rect detectedRect;								// define rectange object
+//	int tilesize = 16;									// tile size of texture classifier (needs to become function argument)
+//	resolution origRes = { 2592, 1458 };					// orignal resolution (needs to become function arguement)
+//
+//															// Step 1: Find contours in image
+//	cv::cvtColor(im, im, cv::COLOR_BGR2GRAY);			// Convert BGR to grayscale findContours
+//	cv::findContours(im, contour_arr, hierarch, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE); // find all contours in image (alt methods available)
+//
+//																						   // Step 2: Find Bounds and Draw on Image
+//	detectedRect = cv::boundingRect(contour_arr[0]);	// get the bounding rectange dimensions from first contour found
+//	cv::resize(imRect, imRect, cv::Size(origRes.width, origRes.height));	// resize image to original size
+//	cv::Rect scaledRect(detectedRect.x*tilesize, detectedRect.y*tilesize, detectedRect.width*tilesize, detectedRect.height*tilesize); // scale detect rectangle to new resolution for distance calc
+//																																	  // Draw Bounding Rectangle on Image
+//	cv::rectangle(imRect, scaledRect, cv::Scalar(0, 0, 255), 3, cv::LINE_8, 0);
+//	cv::Mat imRectView = pyrDownDef(imRect, 3);
+//	cv::imshow("Bounding Rectangle", imRectView);
+//	cv::waitKey(0);
+//
+//	// Step 2 Alternate Display: Draw All Contours
+//	cv::drawContours(imdrawContour, contour_arr, 0, cv::Scalar(0, 0, 255), 3, cv::LINE_8, hierarch);
+//	cv::resize(imdrawContour, imdrawContour, cv::Size(origRes.width, origRes.height));	// resize image to original size
+//	cv::Mat imContourView = pyrDownDef(imdrawContour, 3);
+//	cv::imshow("Detected Contours Drawn On Image", imContourView);
+//	cv::waitKey(0);
+//	// Draw Multiple Contours Detected
+//	//int idx = 0;
+//	//for (; idx >= 0; idx = hierarch[idx][0])
+//	// cv::drawContours(imdraw, contour_arr, idx, cv::Scalar(0,0,255), 3, 8, hierarch);
+//
+//	// Close All Windows
+//	cv::destroyAllWindows();
+//
+//	return rectWidth = scaledRect.width; // returned scaled width to match with focal length
+//}
